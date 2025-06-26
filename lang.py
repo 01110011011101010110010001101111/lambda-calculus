1ed1c30addc93490fa6252a65812fd40b8cd60b2
GenericP3rson <genericp3rson@gmail.com>
2025-06-26 07:51:40 -0400

    saving work before i mess things up

--- lang.py
+++ lang.py
@@ -5,7 +5,7 @@ STEP3: add remaining T/F/etc, with tests!
 STEP4: TYPECHECK! (try to get an exhaustiveness test to work)
 (STRETCH) STEP5: TRY TO COMPILE DOWN TO PYTHON
 
- STEP6: copy dataclass
+STEP6: copy dataclass
 """
 
 from abc import ABC
@@ -28,7 +28,23 @@ class Conditional(Expr):
     t_branch: Expr
     f_branch: Expr
 
-@dataclass 
+@dataclass
+class ConstZero(Expr):
+    pass
+
+@dataclass
+class Successor(Expr):
+    num: Expr
+
+@dataclass
+class Predecessor(Expr):
+    num: Expr
+
+@dataclass
+class ZeroTest(Expr):
+    num: Expr
+
+@dataclass
 class Lambda(Expr):
     arg: str
     body: Expr
@@ -42,15 +58,9 @@ class Evaluation(Expr):
     f: Expr
     a: Expr
 
-# free variables
-def fv(expr: Expr) -> set[str]:
-    match expr:
-        case Variable(name):
-            return {name}
-        case Lambda(arg, body):
-            return fv(body) - arg
-        case Evaluation(f, a):
-            return fv(f) | fv(a)
+@dataclass
+class NumericValue(Expr):
+    num: int
 
 # page 72
 def evaluate(expr: Expr) -> Expr:
@@ -71,6 +81,79 @@ def evaluate(expr: Expr) -> Expr:
                     return Evaluation(fp, t2p)
             else:
                 return Evaluation(fp, a)
+        case ConstTrue() | ConstFalse() | ConstZero():
+            return expr
+        # page 34
+        case Conditional(cond, t_branch, f_branch):
+            # reduce the conditional if possible
+            eval_cond = evaluate(cond)
+            if isinstance(eval_cond, ConstTrue):
+                return evaluate(t_branch)
+            elif isinstance(eval_cond, ConstFalse):
+                return evaluate(f_branch)
+            else:
+                # NOTE: not 1-to-1 with the book, doing unnecessary evals of the t_branch and f_branch
+                # return Conditional(eval_cond, evaluate(t_branch), evaluate(f_branch))
+                return Conditional(eval_cond, t_branch, f_branch)
+        # TODO: MUST BE EXPRESSION!!!
+        case Predecessor(num):
+            # match num:
+            #     # pred 0 -> 0
+            #     case ConstZero():
+            #         return ConstZero()
+            #     # pred ( succ (nv_1) ) -> nv1
+            #     case Successor(nv1) if isinstance(nv1, NumericValue):
+            #         return nv1
+            # t1 -> t1' ==> pred t1 -> pred t1'
+            eval_num = evaluate(num)
+            match eval_num:
+                case NumericValue(_num):
+                    if _num == 1:
+                        return ConstZero()
+                    else:
+                        return NumericValue(_num - 1)
+                case ConstZero():
+                    return ConstZero()
+                case _:
+                    raise NotImplementedError
+        case Successor(num):
+            eval_num = evaluate(num)
+            match eval_num:
+                case NumericValue(_num):
+                    if _num == -1:
+                        return ConstZero()
+                    else:
+                        return NumericValue(_num + 1)
+                case ConstZero():
+                    return NumericValue(1)
+                case _:
+                    raise NotImplementedError
+        case ZeroTest(num):
+            return ConstTrue() if isinstance(evaluate(num), ConstZero) else ConstFalse()
+        case _:
+            raise NotImplementedError
+
+# free variables
+def fv(expr: Expr) -> set[str]:
+    match expr:
+        # main terms
+        case Variable(name):
+            return {name}
+        case Lambda(arg, body):
+            return fv(body) - arg
+        case Evaluation(f, a):
+            return fv(f) | fv(a)
+
+        # TODO -- makes sense but verify
+        case Conditional(cond, t_branch, f_branch):
+            return fv(cond) | fv(t_branch) | fv(f_branch)
+        case ConstTrue() | ConstFalse() | ConstZero():
+            return set()
+        case NumericValue() | ZeroTest() | Successor() | Predecessor():
+            return set()
+
+        case _:
+            raise NotImplementedError
 
 # page 71, section 5.3.5
 def substitute(x: str, s: Expr, expr: Expr) -> Expr:
@@ -81,7 +164,19 @@ def substitute(x: str, s: Expr, expr: Expr) -> Expr:
             return expr
         case Lambda(arg, body) if arg != x and arg not in fv(s):
             return Lambda(y, substitute(x, s, body))
+        case Lambda(arg, body):
+            return expr
         case Evaluation(f, a):
             return Evaluation(substitute(x, s, f), substitute(x, s, a))
-        case _:
+
+        # TODO -- makes sense but verify
+        case Conditional(cond, t_branch, f_branch):
             return expr
+        case ConstTrue() | ConstFalse() | ConstZero():
+            return expr
+        case NumericValue() | ZeroTest() | Successor() | Predecessor():
+            return expr
+
+        case _:
+            raise NotImplementedError
+
--- test.py
+++ test.py
@@ -1,3 +1,27 @@
 from lang import *
 
+def test_ch_3_1():
+    # if false then 0 else 1;
+    cond_result = evaluate(Conditional(ConstFalse(), ConstZero(), Successor(ConstZero())))
+    assert isinstance(cond_result, NumericValue)
+    assert cond_result.num == 1
+
+    # iszero (pred (succ 0));
+    assert isinstance(evaluate(ZeroTest(Predecessor(Successor(ConstZero())))), ConstTrue)
+
+def test_major():
+    # with a variable
+    # x := 5
+    result = evaluate(Variable("a"))
+    assert isinstance(result, Variable)
+    assert result.name == "a"
+
+    result = evaluate(Evaluation(Lambda("x", (Variable("x"))), ConstZero()))
+
+    print(result)
+
+test_major()
+
+# TODO: format this better, etc.
+# test_ch_3_1()
 
